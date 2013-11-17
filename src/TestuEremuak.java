bd4dc671805aeaa31ce4bb877e3293152e0781e5
Juanan Pereira <juanan.pereira@ehu.es>
2013-11-17 16:18:34 +0100

    TestuEremuak is more like the original design

--- src/TestuEremuak.java
+++ src/TestuEremuak.java
@@ -2,25 +2,38 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TestuEremuak extends JFrame {
-  JLabel jLabel1 = new JLabel();
+  JLabel jLabel1 = new JLabel("Sartu zure izena:", SwingConstants.LEFT);
   JTextField jTextField1 = new JTextField();
-  JLabel jLabel2 = new JLabel();
+  JLabel jLabel2 = new JLabel("Utzi zure mezua:", SwingConstants.LEFT);
   JTextArea jTextArea1 = new JTextArea();
 
   public TestuEremuak(){
+
 	this.setTitle("Testu-Eremuekin adibidea");
-   	jLabel1.setText("Sartu zure izena");
-   	jTextField1.setColumns(25);
-	jLabel2.setText("Utzi zure mezua");
-	jTextArea1.setColumns(50);
-   	jTextArea1.setRows(10);
-	this.getContentPane().add(jTextArea1, BorderLayout.SOUTH);
-  	this.getContentPane().add(jLabel2, BorderLayout.NORTH);
- 	this.getContentPane().add(jTextField1, BorderLayout.CENTER);
-   	this.getContentPane().add(jLabel1, BorderLayout.EAST);  
+	JPanel internalPanel = (JPanel) this.getContentPane();
+	
+	jTextArea1.setMaximumSize( new Dimension ( 150, 30));
+	jTextArea1.setColumns(10);	
+	jTextArea1.setLineWrap(true);
+
+	JPanel goia = new JPanel();
+	goia.setAlignmentX(Component.LEFT_ALIGNMENT);
+	goia.setLayout(new BoxLayout(goia, BoxLayout.X_AXIS));
+   	goia.add(jLabel1);  
+ 	goia.add(jTextField1);
+
+
+	JPanel behea = new JPanel(new GridLayout(2,1,10,10));
+	behea.add(jLabel2);
+	behea.add(jTextArea1);	
+
+	internalPanel.add(goia, BorderLayout.NORTH);	
+	internalPanel.add(behea, BorderLayout.CENTER);	
+	this.pack();
   }
  public static void main(String[] args){
     TestuEremuak frame = new TestuEremuak();
+    frame.setDefaultLookAndFeelDecorated(true);
     frame.setSize(300,200);
     frame.setVisible(true);
  }
